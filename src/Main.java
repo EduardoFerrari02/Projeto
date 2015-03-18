a64f6fa299e6446f3f1af73436caffc568f3dab3
eduardo <eduardo___ferrrari@outlook.com>
2015-03-18 14:48:25 -0300

    Teste 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import unip.programa.config.Config;
 import unip.programa.grafico.JanelaEntrada;
 
 public class Main {
-
+   //Teste
 	public static void main(String args[]) throws InterruptedException {
 		/*
 		 * byte[] textoencriptado =
